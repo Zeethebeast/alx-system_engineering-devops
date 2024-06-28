b98164a5025d6fc34dc7dc99dd6d27e4ee1c8475
zeethebeast <chimasamuel6@gmail.com>
2024-06-28 09:10:20 -0700

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+##this file is basically a puppet file that creates file in temp
+file { 'school':
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
+
