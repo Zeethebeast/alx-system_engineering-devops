1aa482881f817dd30040d84680b364bcfe64a883
zeethebeast <chimasamuel6@gmail.com>
2024-06-28 10:39:35 -0700

    puppets

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 ##A puppet file  that installs flask, pyhton and werkzueg
 package { 'flask':
   ensure   => '2.1.0',
+  name     => 'flask',
   provider => 'pip3',
 }
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'werkzeug',
+  provider => 'pip3',
+}
