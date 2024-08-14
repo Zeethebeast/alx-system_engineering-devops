ca4d088c7edcca090b0b79aa9e5f525460ac40e0
zeethebeast <chimasamuel6@gmail.com>
2024-08-14 04:00:24 +0100

    API ADVANCED

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,51 @@
 #!/usr/bin/python3
 """
-this script uses Reddit API to give the number of subscribers """
-
-import json
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
     headers = {
-        'User-Agent': 'myRedditAp/0.0.1'
+        'User-Agent': u_agent
     }
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        return data.get('data', {}).get('subscribers')
-    else:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
+
+# #!/usr/bin/python3
+# """
+# this script uses Reddit API to give the number of subscribers """
+
+# import json
+# import requests
+
+
+# def number_of_subscribers(subreddit):
+#     headers = {
+#         'User-Agent': 'myRedditAp/0.0.1'
+#     }
+
+#     url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+#     response = requests.get(url, headers=headers, allow_redirects=False)
+
+#     if response.status_code == 200:
+#         data = response.json()
+
+#         return data.get('data', {}).get('subscribers')
+#     else:
+#         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,64 @@
 #!/usr/bin/python3
 """
-Gives limit of top ten titles of any subreddit
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
 """
 
-
-import json
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'myRedditApp/0.0.1'}
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post['data']['title'])
-            else:
-                print(None)
-        else:
-            print(None)
-    except requests.RequestException:
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
         print(None)
+        return
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
+# #!/usr/bin/python3
+# """
+# Gives limit of top ten titles of any subreddit
+# """
+
+
+# import json
+# import requests
+
+
+# def top_ten(subreddit):
+#     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+#     headers = {'User-Agent': 'myRedditApp/0.0.1'}
+
+#     try:
+#         response = requests.get(url, headers=headers, allow_redirects=False)
+#         if response.status_code == 200:
+#             posts = response.json().get('data', {}).get('children', [])
+#             if posts:
+#                 for post in posts:
+#                     print(post['data']['title'])
+#             else:
+#                 print(None)
+#         else:
+#             print(None)
+#     except requests.RequestException:
+#         print(None)
