f164465111b41b9bcaa7d0be7f2caf81cedab646
zeethebeast <chimasamuel6@gmail.com>
2024-08-14 03:47:22 +0100

    API ADVANCED

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,6 +18,6 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
 
-        return data.get('data', {}).get('subscribers', 0)
+        return data.get('data', {}).get('subscribers')
     else:
         return 0
